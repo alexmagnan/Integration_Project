3d334e6b4d82971cfea1434d66052f1e6fb9c695
alexmagnan <1062886@15P327-S15-04.studentservers.JAC-SERVERS.JOHNABBOTT.QC.CA>
2015-11-09 12:42:45 -0500

    Not finished search
    didnt finish search

---
+++ MVCBikesWebpage_4A/MVCBikesWebpage/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBikesWebpage.Controllers
+{
+    public class SearchController : Controller
+    {
+        private DBEntities db = new DBEntities();
+        // GET: Search
+        public ActionResult Index(string search)
+        {
+            var products = (from prod in db.Products
+                           join
+                               cate in db.ProductCategories on
+                               prod.ProductCategoryID equals cate.ProductCategoryID
+                           where prod.Name == search || prod.ProductModel.Name == search || cate.Name == search
+                           select prod).Distinct();
+            return View(products.ToList());
+        }
+    }
+}
